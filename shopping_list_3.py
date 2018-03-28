20ea1e64ce96f26b658e37464018742f6067b5d4
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-03-28 16:17:28 -0700

    Initial commit

---
+++ shopping_list_3.py
@@ -0,0 +1,10 @@
+shopping_list = []
+
+
+def show_help():
+    print("What should we pick up at the store?")
+    print("""
+ENTER 'DONE' to stop adding items.
+ENTER 'HELP' for this help.
+ENTER 'SHOW' to see your current list.
+""")
