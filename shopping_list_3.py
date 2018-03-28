ff5adfc393e1415f8c829673a4ac52638844cecd
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-03-28 16:42:26 -0700

    Bug fixes

--- shopping_list_3.py
+++ shopping_list_3.py
@@ -6,7 +6,9 @@ shopping_list = []
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
+
 def show_help():
+    clear_screen()
     print("What should we pick up at the store?")
     print("""
 ENTER 'DONE' to stop adding items.
@@ -16,14 +18,37 @@ ENTER 'SHOW' to see your current list.
 
 
 def add_to_list(item):
-    shopping_list.append(new_item)
-    print("Added! List has {} items.".format(len(shopping_list)))
+    show_list()
+    if len(shopping_list):
+        position = input("Where should I add {}?\n"
+                        "Press ENTER to add to the end of the list\n"
+                        "> ".format(item))
+    else:
+        position = 0
+
+    try: 
+        position = abs(int(position))
+    except ValueError:
+        position = None
+    if position is not None:   
+        shopping_list.insert(position-1, item)
+    else:
+        shopping_list.append(new_item)
+
+    show_list()
 
 
 def show_list():
+    clear_screen()
+
     print("Here's your list:")
+
+    index =1
     for item in shopping_list:
-        print(item)
+        print("{}. {}".format(index, item))
+        index += 1
+
+    print("-"*10)
 
 
 show_help()
@@ -31,12 +56,12 @@ show_help()
 while True:
     new_item = input("> ")
 
-    if new_item == 'DONE':
+    if new_item.upper() == 'DONE' or new_item.upper() == 'QUIT':
         break
-    elif new_item == 'HELP':
+    elif new_item.upper() == 'HELP':
         show_help()
         continue
-    elif new_item == 'SHOW':
+    elif new_item.upper() == 'SHOW':
         show_list()
         continue
 
