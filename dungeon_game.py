4cb945464b864b2ca79f5f9cf1a2fb51f013bfd2
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-04-01 17:27:11 -0700

    Added move function

--- dungeon_game.py
+++ dungeon_game.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 # draw grid
@@ -17,14 +18,14 @@ CELLS = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
          (0, 3), (1, 3), (2, 3), (3, 3), (4, 3),
          (0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]   
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def get_locations():
-    monster = None
-    door = None
-    player = None
+    return random.sample(CELLS, 3)
 
-    return monster, door, player
+
 
 def move_player(player, move):
     # get the player's location
@@ -36,17 +37,24 @@ def move_player(player, move):
 
 def get_moves(player):
     moves = ["LEFT", "RIGHT", "UP", "DOWN"]
-    # if player's y == 0, they can't move up
-    # if player's y == 4, they can't move down (grid limit)
-    # if player's x == 0, they can't move left
-    # if player's x == 4, they can't move right
+    x, y = player
+    if x == 0:  # if player's x == 0, they can't move left
+        moves.remove("LEFT")
+    if x == 4:  # if player's x == 4, they can't move right
+        moves.remove("RIGHT")
+    if y == 0:  # if player's y == 0, they can't move up
+        moves.remove("UP")
+    if y == 4:  # if player's y == 4, they can't move down (grid limit)
+        moves.remove("DOWN")
     return moves
+
+moster, door, player = get_locations()
 
 
 while True:
     print("Welcome to the dungion!")
-    print("You're currently in room {}") # fill with player position
-    print("You can move {}") # fill with available moves
+    print("You're currently in room {}".format(player)) # fill with player position
+    print("You can move {}".format(", ".join(get_moves(player)))) # fill with available moves
     print("Enter QUIT to quit")
 
     move = input("> ")
